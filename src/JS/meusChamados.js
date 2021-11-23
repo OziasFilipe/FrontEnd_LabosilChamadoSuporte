29ce264e21661d229144b51738eedadd7383e318
OziasFilipe <43150504+OziasFilipe@users.noreply.github.com>
2021-11-23 18:25:09 -0300

    Add files via upload

--- src/JS/meusChamados.js
+++ src/JS/meusChamados.js
@@ -1,42 +1,75 @@
 var boxConteiner = document.getElementsByClassName("boxConteiner")
-var UrlGet = "http://localhost:8080/admProtocoloChamado/";
-var UrlGetAll = "http://localhost:8080/basicNomeUsuario?basicNomeUsuario=ozias filipe";
+var UrlGet = "http://localhost:8080/admChamadoAberto?admChamadoAberto=";
+var UrlGetProtocolo = "http://localhost:8080/admProtocoloChamado/"
+var UrlGetAll = "http://localhost:8080/basicNomeUsuario?basicNomeUsuario=filipe";
 var numeroChamado = document.getElementById("numeroChamado");
 
-var aberturaChamado = document.getElementById("aberturaChamado")
-var teste = aberturaChamado.appendChild("<p></p>")
-console.log(aberturaChamado)
+
+//elementos html 
+var divFlexDirection = document.getElementById('divFlexDirection')
+console.log(divFlexDirection)
+// controle inner html
+let tbody = document.getElementById("tbody")
+//console.log(documento.innerHTML = txt)
+
+
+////////////////////////////////////
 
 //Variaveis GetValor
 let data_chamado = ""
 let fechamentoDoChamado = ""
 let Descricao = ""
 let resolucao = ""
+var select = document.getElementById('selectSituacao')
+let teste = document.getElementById("tbody")
+console.log(teste)
 
+console.log(select.value)
+
+//Select de busca 
 function BuscarChamado(){
-    
-    if(numeroChamado.value != ""){
-
-        fetch(UrlGet+numeroChamado.value).then(response=>{
-            return response.json()
-        }).then(data=>{
-            console.log(data[0])
-            if(data[0] == undefined){
-                alert("Codigo não foi encontrado!")
-            }else{
-                data_chamado = data[0].data_chamado
-                console.log(aberturaDoChamado)
+    fetch(UrlGet+select.value).then(response =>{
+        return response.json();
+          }).then(data =>{ 
+              let dados = []
+            for(let i = 0; i < data.length; i++){
+                var protocolo = data[i].protocolo_chamado
+                var th = `<tr class="tbody-white" onclick="InserirDados(${protocolo})">
+                <th scope="row">1</th>
+                <td>${data[i].protocolo_chamado}</td>
+                <td>${data[i].data_chamado}</td>
+                <td>${data[i].tipo_chamado}</td>
+                <td>${data[i].andamento_chamado}</td>
+                </tr>`
+                dados.push(th)
             }
-        })
-    }
-    else{
-        fetch(UrlGetAll).then(response=>{
-            return response.json()
-        }).then(data=>{
-            console.log(data)
-            if(data[0] == undefined){
-                alert("Codigo não foi encontrado!")
-            }
-        })
-    }
+            tbody.innerHTML = dados
+      })
 }
+//Selecionar bloco
+function InserirDados(protocolo){
+    console.log(protocolo)
+    fetch(UrlGetProtocolo+protocolo).then(response =>{
+        return response.json();
+          }).then(data =>{   
+              console.log(data)
+      })
+}
+//Iniciar em aberto
+fetch(UrlGet+select.value).then(response =>{
+    return response.json();
+      }).then(data =>{ 
+          let dados = []
+        for(let i = 0; i < data.length; i++){
+            var protocolo = data[i].protocolo_chamado
+            var th = `<tr class="tbody-white" onclick="InserirDados(${protocolo})">
+            <th scope="row">1</th>
+            <td>${data[i].protocolo_chamado}</td>
+            <td>${data[i].data_chamado}</td>
+            <td>${data[i].tipo_chamado}</td>
+            <td>${data[i].andamento_chamado}</td>
+            </tr>`
+            dados.push(th)
+        }
+        tbody.innerHTML = dados
+  })
